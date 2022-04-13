d7d6853df127d69aad9d4ea4452a469a5a24a22f
etahinnocent360 <etahinno360@gmail.com>
2022-04-13 23:54:56 +0100

    db url

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,7 @@ import { HeadersMiddlware } from './Headers.MiddleWare';
       load: [configurations],
       validationSchema,
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/transactions'),
+    MongooseModule.forRoot(process.env.DB_URL),
     MongooseModule.forFeature([
       { name: 'getTransaction ', schema: getTransactionSchema },
       { name: 'getPsp', schema: getPspSchema },
