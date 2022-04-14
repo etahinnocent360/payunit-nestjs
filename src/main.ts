f0114aebb293b0fd898ce92b8720af11eeb60198
etahinnocent360 <etahinno360@gmail.com>
2022-04-14 17:46:41 +0100

    added cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import helmet from 'helmet';
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(helmet());
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
