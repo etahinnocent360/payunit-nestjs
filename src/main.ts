1712898e8517004f85318b0d7a656bac0aedcccf
etahinnocent360 <etahinno360@gmail.com>
2022-04-14 17:38:17 +0100

    added helmet

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Cores } from 'cores';
-import { ConfigService } from '@nestjs/config';
-import * as session from 'express-session';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.enableCors();
-  app.use(
-    session({
-      secret: 'hhjsdugyhjkdssd',
-      resave: false,
-      saveUninitialized: true,
-      cookie: {
-        maxAge: 60000,
-      },
-    }),
-  );
-  const config = app.get(ConfigService);
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
